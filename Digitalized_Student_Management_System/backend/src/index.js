7a3d451db55712db67faa992492f2b686ede43aa
Shrey <163460041+Shrey-Sawant@users.noreply.github.com>
2024-08-30 02:45:27 +0530

    sign up credentials

--- Digitalized_Student_Management_System/backend/src/index.js
+++ Digitalized_Student_Management_System/backend/src/index.js
@@ -1,6 +1,7 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
 import { app } from "./app.js";
+import User from "./models/user.models.js"
 
 dotenv.config()
 
@@ -12,4 +13,10 @@ connectDB()
 })
 .catch((err)=>{
     console.log("MONGODB CONNECTION FAILED!!",err);
+})
+
+app.post('/register',(req,res)=>{
+    User.create(req.body)
+    .then(users => res.json(users))
+    .catch(err => res.json(err))
 })
