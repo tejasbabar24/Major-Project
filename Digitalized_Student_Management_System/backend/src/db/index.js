f45b7762feff84e4124e265b3592936bdb02059e
Shrey <163460041+Shrey-Sawant@users.noreply.github.com>
2024-10-05 00:17:06 +0530

    Backend_Register_controller_and_Routes_Incomplete

--- Digitalized_Student_Management_System/backend/src/db/index.js
+++ Digitalized_Student_Management_System/backend/src/db/index.js
@@ -1,14 +1,14 @@
 import mongoose from "mongoose";
-import {DB_NAME} from "../constants.js";
+import { DB_NAME } from "../constants.js";
 
-const connectDB = async()=>{
+const connectDB = async () => {
     try {
-        const connectionInstance=await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
         console.log(`\n MONGODB Connected!!! DB_Host:${connectionInstance.connection.host}`);
     } catch (error) {
-        console.log("MONGODB Connection Error",error);
+        console.log("MONGODB Connection Error", error);
         process.exit(1);
     }
 }
 
-export default connectDB
+export default connectDB;
