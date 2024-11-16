dc85727d12b0ef31cc3b2f62ad3962c7e249851b
Tejasbabar24 <tejasbabar864@gmail.com>
2024-11-16 19:28:08 +0530

    done the student attendance part full

--- Digitalized_Student_Management_System/fronend/src/components/attendance/ShowAttendace.jsx
+++ Digitalized_Student_Management_System/fronend/src/components/attendance/ShowAttendace.jsx
@@ -7,40 +7,42 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
+import { useSelector } from 'react-redux';
 
-const StyledTableCell = styled(TableCell)(({ theme }) => ({
-  [`&.${tableCellClasses.head}`]: {
-    backgroundColor: theme.palette.common.black,
-    color: theme.palette.common.white,
-  },
-  [`&.${tableCellClasses.body}`]: {
-    fontSize: 14,
-  },
-}));
+export default function ShowAttendance({ dates, presentDates }) {
+  const userData = useSelector((state) => state.auth.userData);
 
-const StyledTableRow = styled(TableRow)(({ theme }) => ({
-  '&:nth-of-type(odd)': {
-    backgroundColor: theme.palette.action.hover,
-  },
-  // hide last border
-  '&:last-child td, &:last-child th': {
-    border: 0,
-  },
-}));
+  // Extract the dates from the presentDates array
+  const presDates = presentDates.map((item) => item.Date);
 
-function createData(name, calories, fat,) {
-  return { name, calories, fat,  };
-}
+  // Styled components for table cells and rows
+  const StyledTableCell = styled(TableCell)(({ theme }) => ({
+    [`&.${tableCellClasses.head}`]: {
+      backgroundColor: theme.palette.common.black,
+      color: theme.palette.common.white,
+    },
+    [`&.${tableCellClasses.body}`]: {
+      fontSize: 14,
+    },
+  }));
 
-const rows = [
-  createData('10-11-24', 0, 1, ),
-  createData('14-11-24', 1, 0, ),
-  createData('24-11-24', 1, 0,),
-  createData('28-11-24', 0, 1, ),
-  createData('29-11-24', 1, 0,),
-];
+  const StyledTableRow = styled(TableRow)(({ theme }) => ({
+    '&:nth-of-type(odd)': {
+      backgroundColor: theme.palette.action.hover,
+    },
+    '&:last-child td, &:last-child th': {
+      border: 0,
+    },
+  }));
 
-export default function ShowAttendance() {
+  // Dynamically create data for attendance table
+  const createData = (date) => {
+    return { date };
+  };
+
+  // Map through dates to create rows dynamically
+  const rows = dates.map((date) => createData(date));
+
   return (
     <TableContainer component={Paper}>
       <Table sx={{ minWidth: 700 }} aria-label="customized table">
@@ -49,18 +51,23 @@ export default function ShowAttendance() {
             <StyledTableCell>Date (Lectures Conducted)</StyledTableCell>
             <StyledTableCell align="right">Present</StyledTableCell>
             <StyledTableCell align="right">Absent&nbsp;</StyledTableCell>
-            
           </TableRow>
         </TableHead>
         <TableBody>
           {rows.map((row) => (
-            <StyledTableRow key={row.name}>
+            <StyledTableRow key={row.date}>
               <StyledTableCell component="th" scope="row">
-                {row.name}
+                {row.date}
               </StyledTableCell>
-              <StyledTableCell align="right">{row.calories}</StyledTableCell>
-              <StyledTableCell align="right">{row.fat}</StyledTableCell>
-              
+
+              {/* Check if the row.date is in the presDates array */}
+              <StyledTableCell align="right">
+                {presDates.includes(row.date) ? 'Present' : '-'}
+              </StyledTableCell>
+
+              <StyledTableCell align="right">
+                {!presDates.includes(row.date) ? 'Absent' : '-'}
+              </StyledTableCell>
             </StyledTableRow>
           ))}
         </TableBody>
