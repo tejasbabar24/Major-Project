b271092b7981469d35093f5ae8fad201df3fe2ae
SwapnilShinde20 <shindeswapnil2005@gmail.com>
2024-11-27 10:47:39 +0530

    proxy

--- Digitalized_Student_Management_System/fronend/vite.config.js
+++ Digitalized_Student_Management_System/fronend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server:{
     proxy:{
-      "/api":"http://localhost:8000"
+      "/api":"https://major-project-y6c7.onrender.com"
     }
   }
 })
